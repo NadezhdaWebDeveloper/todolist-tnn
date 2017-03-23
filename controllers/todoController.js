2445823e98978bb47d536271c5373790f68ea1a2
NadezhdaWebDeveloper <nadezhda2991@gmail.com>
2017-03-23 17:53:00 +0200

    Add/Delete item to/from mongodb

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -11,13 +11,9 @@ var todoSchema = new mongoose.Schema({
 
 //Create a model (collection)
 var Todo = mongoose.model('Todo', todoSchema);
-var todoOne = Todo( { item: 'buy flowers'} ).save(function(err) {
-  if (err) throw err;
-  console.log('item saved');
-});
 
-
-var data = [{item: 'to read a book'}, {item: 'to go to the market'}, {item: 'to write a letter'}];
+// simulation database
+// var data = [{item: 'to read a book'}, {item: 'to go to the market'}, {item: 'to write a letter'}];
 
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -25,20 +21,41 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 module.exports = function(app) {
 
   app.get('/todo', function(req, res) {
-    res.render('todo', {todos: data});
+    // simulation database
+    // res.render('todo', {todos: data});
+
+    // get data from mongodb and pass it to view
+    Todo.find({}, function(err, data){
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    });
   });
 
   app.post('/todo', urlencodedParser, function(req, res) {
-    if (!req.body) return res.sendStatus(400);
-    data.push(req.body);
-    res.json(data);
+    // simulation database
+    // if (!req.body) return res.sendStatus(400);
+    // data.push(req.body);
+    // res.json(data);
+
+    // get data from the view and add it to mongodb
+    var newTodo = Todo(req.body).save(function(err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function(req, res) {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    // simulation database
+    // data = data.filter(function(todo) {
+    //   return todo.item.replace(/ /g, '-') !== req.params.item;
+    // });
+    // res.json(data);
+
+    // delete the requested item from mongodb
+    Todo.find({ item: req.params.item.replace(/\-/g, ' ') }).remove(function(err, data) {
+      if (err) throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 
 };
